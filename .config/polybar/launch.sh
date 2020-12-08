9a32bb4ea04f722145f2a08fab3bbedbc053fa73
papuSpartan <mcgpapu@gmail.com>
2020-12-07 18:36:59 -0600

    a lot

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,9 +9,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar rightmon -c ~/.config/polybar/config-bottom.ini 2>/home/papu/.config/polybar/log &
+polybar rightmon -r -c ~/.config/polybar/bar.ini 2>/home/papu/.config/polybar/log &
 
-polybar leftmon -c ~/.config/polybar/config-bottom.ini 2>/home/papu/.config/polybar/log &
+
+#polybar leftmon -r -c ~/.config/polybar/config-bottom.ini 2>/home/papu/.config/polybar/log &
 
 
 
