11082000ff00bc77d525c8dfe26a8f5b35668e51
papuSpartan <mcgpapu@gmail.com>
2021-01-11 04:31:22 -0600

    a lot

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,8 +9,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar rightmon -r -c ~/.config/polybar/bar.ini 2>/home/papu/.config/polybar/log &
+ polybar rightmon -r -c ~/.config/polybar/bar.ini 2>/home/papu/.config/polybar/log &
 
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload rightmon &
+#  done
+#else
+#  polybar --reload rightmon &
+#fi
 
 #polybar leftmon -r -c ~/.config/polybar/config-bottom.ini 2>/home/papu/.config/polybar/log &
 
